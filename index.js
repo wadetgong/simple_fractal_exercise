3964dda3ac144e17ccb41743769f08825e4c86bb
Wade Gong <wadetgong@gmail.com>
2017-11-01 08:04:35 -0500

    updates port logic

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.use((req, res, next, err) => {
   console.error(err)
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server listening on port 3000.')
 })
